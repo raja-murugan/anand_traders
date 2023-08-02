25bf18f249e8af01d3e9206c90fb05cd164fcbf3
Rajamurugan Thirumaran <muruganraja335@gmail.com>
2023-08-02 11:40:17 +0530

    first commit

---
+++ resources/views/layout/backend/components/auth/script.blade.php
@@ -0,0 +1,24 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
+
+<script src="{{ asset('assets/backend/js/jquery-3.7.0.min.js') }}"></script>
+
+<script src="{{ asset('assets/backend/js/feather.min.js') }}"></script>
+
+<script src="{{ asset('assets/backend/js/jquery.slimscroll.min.js') }}"></script>
+
+<script src="{{ asset('assets/backend/js/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('assets/backend/js/dataTables.bootstrap4.min.js') }}"></script>
+
+<script src="{{ asset('assets/backend/js/bootstrap.bundle.min.js') }}"></script>
+
+<script src="{{ asset('assets/backend/plugins/apexchart/apexcharts.min.js') }}"></script>
+<script src="{{ asset('assets/backend/plugins/apexchart/chart-data.js') }}"></script>
+
+<script src="{{ asset('assets/backend/js/script.js') }}"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+<script>
+    $(document).ready(function() {
+        $('.js-example-basic-single').select2();
+    });
+</script>
---
+++ resources/views/layout/backend/components/auth/side-bar.blade.php
@@ -0,0 +1,70 @@
+<div class="sidebar" id="sidebar">
+    <div class="sidebar-header">
+        <div class="sidebar-logo">
+            <a href="index.html">
+                <img src="{{ asset('assets/backend/img/logo.png') }}" class="img-fluid logo" alt="">
+            </a>
+            <a href="index.html">
+                <img src="{{ asset('assets/backend/img/logo-small.png') }}" class="img-fluid logo-small" alt="">
+            </a>
+        </div>
+    </div>
+    <div class="sidebar-inner slimscroll">
+        <div id="sidebar-menu" class="sidebar-menu">
+
+            <ul>
+                <li class="menu-title"><span>Main</span></li>
+                <li>
+                    <a class="active" href="#"><i class="fe fe-home"></i><span>Dashboard</span></a>
+                </li>
+                <li>
+                    <a href="#"><i class="fe fe-pocket"></i> <span>Bank</span></a>
+                </li>
+                <li>
+                    <a href="#"><i class="fe fe-package"></i> <span>Product</span></a>
+                </li>
+                <li>
+                    <a href="#"><i class="fe fe-book-open"></i> <span>Addon</span></a>
+                </li>
+            </ul>
+
+
+            <ul>
+                <li class="menu-title"><span>User Management</span></li>
+                <li>
+                    <a href="#"><i class="fe fe-users"></i> <span>Customers</span></a>
+                </li>
+                <li>
+                    <a href="#"><i class="fe fe-user"></i> <span>Vendor</span></a>
+                </li>
+            </ul>
+
+            <ul>
+                <li class="menu-title"><span>Bill Management</span></li>
+                <li>
+                    <a href="#"><i class="fe fe-file-text"></i> <span>Quatation</span></a>
+                </li>
+                <li>
+                    <a href="#"><i class="fe fe-database"></i> <span>Bill</span></a>
+                </li>
+                <li>
+                    <a href="#"><i class="fe fe-shopping-cart"></i> <span>Purchase</span></a>
+                </li>
+                <li>
+                    <a href="#"><i class="fe fe-credit-card"></i> <span>Expence</span></a>
+                </li>
+            </ul>
+
+            <ul>
+                <li class="menu-title"><span>Payment Management</span></li>
+                <li>
+                    <a href="#"><i class="fe fe-dollar-sign"></i> <span>Vendor Payment</span></a>
+                </li>
+                <li>
+                    <a href="#"><i class="fe fe-dollar-sign"></i> <span>Customer Payment</span></a>
+                </li>
+            </ul>
+
+        </div>
+    </div>
+</div>
