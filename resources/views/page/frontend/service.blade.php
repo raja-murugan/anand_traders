375ac5166d515b3daef7284a20c47adb7404cc16
Rajamurugan Thirumaran <muruganraja335@gmail.com>
2023-09-29 19:26:15 +0530

    Front End Update

--- resources/views/page/frontend/service.blade.php
+++ resources/views/page/frontend/service.blade.php
@@ -4,11 +4,10 @@
 <!--Page Title-->
 <section class="page-title" style="background-image:url({{ asset('assets/frontend/images/background/1.jpg') }})">
     <div class="auto-container">
-        <h1>Doors Open the Possibilities</h1>
+        <h1>PVC, UPVC, & WPVC Manfacturing</h1>
         <ul class="page-breadcrumb">
             <li><a href="index.html">Home</a></li>
-            <li>Doors</li>
-            <li>Types</li>
+            <li>Service</li>
         </ul>
     </div>
 </section>
@@ -19,7 +18,7 @@
     <div class="auto-container">
         <!--Title Box-->
         <div class="title-box">
-            <h2>Shutters For Doors</h2>
+            <h2>Our Services</h2>
             <div class="text">For a free estimate call us at <span class="theme_color">1800 208 5331</span> or <a href="contact.html">click here</a> to send us a message.</div>
         </div>
 
@@ -38,7 +37,7 @@
                                     <a href="blog-single.html"><img src="images/resource/service-4.jpg" alt="" /></a>
                                 </div>
                                 <div class="lower-content">
-                                    <h2><a href="blog-single.html">Casement</a></h2>
+                                    <h2><a href="blog-single.html">PVC Door & Cupboard</a></h2>
                                     <div class="text">Consequences that are extremely nor of us ever undertakes take a example, of us ever undertakes...</div>
                                     <div class="icon-box">
                                         <span class="icon flaticon-door-1"></span>
@@ -55,7 +54,7 @@
                                     <a href="blog-single.html"><img src="images/resource/service-5.jpg" alt="" /></a>
                                 </div>
                                 <div class="lower-content">
-                                    <h2><a href="blog-single.html">Sliding Door</a></h2>
+                                    <h2><a href="blog-single.html">Mosquito etion</a></h2>
                                     <div class="text">Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because is pain circumstances...</div>
                                     <div class="icon-box">
                                         <span class="icon flaticon-door-3"></span>
@@ -72,7 +71,7 @@
                                     <a href="blog-single.html"><img src="images/resource/service-6.jpg" alt="" /></a>
                                 </div>
                                 <div class="lower-content">
-                                    <h2><a href="blog-single.html">Tilt & slide</a></h2>
+                                    <h2><a href="blog-single.html">Glazing & ACp Work</a></h2>
                                     <div class="text">Explain to you how this mistaken idea of denouncinga pleasure and praising pain account of the system...</div>
                                     <div class="icon-box">
                                         <span class="icon flaticon-door-2"></span>
@@ -89,7 +88,7 @@
                                     <a href="blog-single.html"><img src="images/resource/service-7.jpg" alt="" /></a>
                                 </div>
                                 <div class="lower-content">
-                                    <h2><a href="blog-single.html">Slide & Fold</a></h2>
+                                    <h2><a href="blog-single.html">Aluminium Work</a></h2>
                                     <div class="text">Which toil and pain procure him some great pleasure. To take a trivial example, which ever undertakes...</div>
                                     <div class="icon-box">
                                         <span class="icon flaticon-door-5"></span>
@@ -106,7 +105,7 @@
                                     <a href="blog-single.html"><img src="images/resource/service-8.jpg" alt="" /></a>
                                 </div>
                                 <div class="lower-content">
-                                    <h2><a href="blog-single.html">Lift & Slide</a></h2>
+                                    <h2><a href="blog-single.html">Wooden Cupboard</a></h2>
                                     <div class="text">Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because is pain circumstances...</div>
                                     <div class="icon-box">
                                         <span class="icon flaticon-door-7"></span>
@@ -123,10 +122,61 @@
                                     <a href="blog-single.html"><img src="images/resource/service-9.jpg" alt="" /></a>
                                 </div>
                                 <div class="lower-content">
-                                    <h2><a href="blog-single.html">Custom Models</a></h2>
+                                    <h2><a href="blog-single.html">Modular Kitchen</a></h2>
                                     <div class="text">Consequences that are extremely nor of us ever undertakes take a example, of us ever undertakes...</div>
                                     <div class="icon-box">
                                         <span class="icon flaticon-door-2"></span>
+                                    </div>
+                                    <a class="explore" href="blog-single.html">Explore More <span class="arrow flaticon-right-arrow-2"></span></a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!--Services Block Three-->
+                        <div class="services-block-three col-lg-6 col-md-6 col-sm-12">
+                            <div class="inner-box">
+                                <div class="image">
+                                    <a href="blog-single.html"><img src="images/resource/service-8.jpg" alt="" /></a>
+                                </div>
+                                <div class="lower-content">
+                                    <h2><a href="blog-single.html">Solid PVC & WPVC Door</a></h2>
+                                    <div class="text">Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because is pain circumstances...</div>
+                                    <div class="icon-box">
+                                        <span class="icon flaticon-door-7"></span>
+                                    </div>
+                                    <a class="explore" href="blog-single.html">Explore More <span class="arrow flaticon-right-arrow-2"></span></a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!--Services Block Three-->
+                        <div class="services-block-three col-lg-6 col-md-6 col-sm-12">
+                            <div class="inner-box">
+                                <div class="image">
+                                    <a href="blog-single.html"><img src="images/resource/service-9.jpg" alt="" /></a>
+                                </div>
+                                <div class="lower-content">
+                                    <h2><a href="blog-single.html">Vinyl Floor & Wall Paper</a></h2>
+                                    <div class="text">Consequences that are extremely nor of us ever undertakes take a example, of us ever undertakes...</div>
+                                    <div class="icon-box">
+                                        <span class="icon flaticon-door-2"></span>
+                                    </div>
+                                    <a class="explore" href="blog-single.html">Explore More <span class="arrow flaticon-right-arrow-2"></span></a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!--Services Block Three-->
+                        <div class="services-block-three col-lg-6 col-md-6 col-sm-12">
+                            <div class="inner-box">
+                                <div class="image">
+                                    <a href="blog-single.html"><img src="images/resource/service-8.jpg" alt="" /></a>
+                                </div>
+                                <div class="lower-content">
+                                    <h2><a href="blog-single.html">UPVC Window & Door Manfacturing</a></h2>
+                                    <div class="text">Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because is pain circumstances...</div>
+                                    <div class="icon-box">
+                                        <span class="icon flaticon-door-7"></span>
                                     </div>
                                     <a class="explore" href="blog-single.html">Explore More <span class="arrow flaticon-right-arrow-2"></span></a>
                                 </div>
